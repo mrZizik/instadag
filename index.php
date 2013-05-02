8b3588840784a005fca28f0826f089364d0a6ea9
mrZizik <mrfreezzzer@mail.ru>
2013-05-02 23:48:18 +0400

    кнопки лайк и коммент на главной страницу

--- index.php
+++ index.php
@@ -1,71 +1,4 @@
-
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Insta'Даг - Дагестанская Instagram галерея</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Галерея фотографий связанных с Дагестаном из сервиса Instagram">
-    <meta name="author" content="mrAbdulmajidov">
-
-    <!-- Le styles -->
-    <link href="assets/css/bootstrap.css" rel="stylesheet">
-    <style type="text/css">
-      body {
-        padding-top: 60px;
-        padding-bottom: 40px;
-      }
-      .sidebar-nav {
-        padding: 9px 0;
-      }
-    </style>
-    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
-
-    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-
-    <!-- Fav and touch icons -->
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
-      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
-                    <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
-                                   <link rel="shortcut icon" href="assets/ico/favicon.png">
-  </head>
-
-  <body>
-    <? php include "gallery.php"; ?>
-
-    <div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container-fluid">
-          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </a>
-          <a class="brand" href="#"><img src="assets/img/logo_m.png" /></a>
-          <div class="nav-collapse collapse">
-            <div class="pull-right">
-              <div class="control-group" style="margin-bottom: 0px; margin-right: 3px;">
-              
-                <div class="controls">
-                  <div id="tag-search" class="input-prepend">
-                    <span class="add-on"><i class="icon-tags"></i></span>
-                    <input class="span2" id="inputIcon" type="text">
-                  </div>
-                </div>
-              </div>
-              </div>
-            <ul class="nav">
-              <li><a href="#about">О сайте</a></li>
-              <li><a href="#contact">Контакты</a></li>
-            </ul>
-          </div><!--/.nav-collapse -->
-        </div>
-      </div>
-    </div>
+<?php  include "header.php"; ?>
 
     <div class="container-fluid">
      <div class="row-fluid">
@@ -91,6 +24,9 @@
       if (strlen($shorttext)<=3) {
         $shorttext=substr($text, 0, 54);
       }}
+      else {
+        $shorttext="Нет описания";
+      }
       $id=$response->data[$i]->id;
       
       ?>
@@ -101,7 +37,9 @@
         <p>by: @<?=$author ?></p>
         <a href=imageview.php?id=<?=$i ?>&row=<?=$row ?>><img src=<?=$thumbnail ?>></a>
       <div class="info">
-        <p><i class="icon-heart"></i>: <?=$likes?>. <i class="icon-comment"></i>: <?=$comments?> </p>
+        <div class="btn-group" style="margin-left: 10px; margin-top: 5px;">
+        <a href="/imageview.php?id=<?=$i ?>&row=<?=$row ?>&state=2" class="btn"><i class="icon-heart"></i> <?=$likes ?></a>
+                                <a href="/imageview.php?id=<?=$i ?>&row=<?=$row ?>&state=3" class="btn"><i class="icon-comment"></i> <?=$comments ?></a></div>
         <p><?=$shorttext ?></p>
       </div>
       </div>
@@ -138,7 +76,9 @@
         
             
       <div class="info">
-        <p><i class="icon-heart"></i>: <?=$likes?>. <i class="icon-comment"></i>: <?=$comments?> </p>
+        <div class="btn-group" style="margin-left: 10px; margin-top: 5px;">
+        <a href="/imageview.php?id=<?=$i ?>&row=<?=$row ?>&state=2" class="btn"><i class="icon-heart"></i> <?=$likes ?></a>
+                                <a href="/imageview.php?id=<?=$i ?>&row=<?=$row ?>&state=3" class="btn"><i class="icon-comment"></i> <?=$comments ?></a></div>
         <p><?=$shorttext ?></p>
       </div>  
       </div>
@@ -174,7 +114,9 @@
         <p>by: @<?=$author ?></p>
         <a href=imageview.php?id=<?=$i ?>&row=<?=$row ?> ><img src=<?=$thumbnail ?>></a>
         <div class="info">
-        <p>Нравится: <?=$likes?>. Комментариев: <?=$comments?> </p>
+        <div class="btn-group" style="margin-left: 10px; margin-top: 5px;">
+        <a href="/imageview.php?id=<?=$i ?>&row=<?=$row ?>&state=2" class="btn"><i class="icon-heart"></i> <?=$likes ?></a>
+                                <a href="/imageview.php?id=<?=$i ?>&row=<?=$row ?>&state=3" class="btn"><i class="icon-comment"></i> <?=$comments ?></a></div>
         <p><?=$shorttext ?></p>
       </div>
       </div>
